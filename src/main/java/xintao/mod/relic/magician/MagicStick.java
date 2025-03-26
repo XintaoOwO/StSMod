09b97ad2c8df8bf3c71399bf818846945e72a733
xintao <andymc233@163.com>
2025-03-26 17:38:37 +0800

    25.0326.4

--- src/main/java/xintao/mod/relic/magician/MagicStick.java
+++ src/main/java/xintao/mod/relic/magician/MagicStick.java
@@ -3,15 +3,13 @@ package xintao.mod.relic.magician;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import xintao.mod.util.CodeUtil;
 
 public class MagicStick extends CustomRelic
 {
     public static final String ID = CodeUtil.of("Magic_Stick");
-    private static final String img_path = CodeUtil.imgPath("relic/Magic_Stick.png");
-    private static final String outline_path = CodeUtil.imgPath("relic/Magic_Stick_Outline.png");
+    private static final String img_path = CodeUtil.imgPath("relic/magic_stick.png");
     private static final RelicTier tier = RelicTier.STARTER;
     private static final String description = "每次战斗开始时，";
     private static final LandingSound landing_sound = LandingSound.FLAT;
