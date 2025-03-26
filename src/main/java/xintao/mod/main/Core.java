a043b309a3fa934ad21d333d9962e879c29e5a81
xintao <andymc233@163.com>
2025-03-26 16:16:50 +0800

    25.0326.3

--- src/main/java/xintao/mod/main/Core.java
+++ src/main/java/xintao/mod/main/Core.java
@@ -1,20 +1,14 @@
 package xintao.mod.main;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SpireInitializer
 public class Core
 {
-    public static final Logger logger = LogManager.getLogger(Core.class);
-    
     public static void initialize()
     {
         new Card();
         new Relic();
         new Character();
-        
-        logger.info("Mod Initializing...");
     }
 }
