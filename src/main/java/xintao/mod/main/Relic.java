e3f1e61c3df72556deeac6cea9ec1bc6e0da85cc
xintao <andymc233@163.com>
2025-03-29 23:21:45 +0800

    25.0329.1

--- src/main/java/xintao/mod/main/Relic.java
+++ src/main/java/xintao/mod/main/Relic.java
@@ -1,7 +1,12 @@
 package xintao.mod.main;
 
+//import basemod.AutoAdd;
+
 import basemod.BaseMod;
 import basemod.interfaces.EditRelicsSubscriber;
+//import com.megacrit.cardcrawl.unlock.UnlockTracker;
+//import xintao.mod.character.Magician;
+//import xintao.mod.util.CodeUtil;
 
 public class Relic implements EditRelicsSubscriber
 {
@@ -15,22 +20,22 @@ public class Relic implements EditRelicsSubscriber
     {
         //BaseMod.addRelic(new MagicStick(), RelicType.SHARED);
         
-        /*
+        
         // This finds and adds all relics inheriting from CustomRelic that are in the same package
         // as MyRelic, keeping all as unseen except those annotated with @AutoAdd.Seen
+        /*
         new AutoAdd(CodeUtil.Mod_ID)
                 .packageFilter(CodeUtil.Mod_Relic_Package + ".magician")
-                .any(CustomRelic.class, (info, relic) -> 
+                .any(CustomRelic.class, (info, relic) ->
                         {
                             BaseMod.addRelicToCustomPool(relic, Magician.PlayerColorEnum.Magician_Colour);
-                            
+
                             if (info.seen)
                             {
                                 UnlockTracker.markRelicAsSeen(relic.relicId);
                             }
                         }
                 );
-                
          */
     }
 }
