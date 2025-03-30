461dffb3e0c4baec2132bf004e5d21cdefd7cc2b
xintao <andymc233@163.com>
2025-03-30 21:22:36 +0800

    25.0330.2

--- src/main/java/xintao/mod/card/magician/Cheat.java
+++ src/main/java/xintao/mod/card/magician/Cheat.java
@@ -17,19 +17,19 @@ public class Cheat extends CustomCard
     public static final String ID = CodeUtil.of("Cheat");
     private static final String name = "出千";
     private static final int cost = 1;
-    private static final String description1 = "* 预见 5。 NL 抽2张牌。 NL 丢弃1张牌。";
-    private static final String description2 = "* 预见 5。 NL 抽3张牌。 NL 丢弃任意张牌。";
+    private static final String description1 = "* 预见 !M! 。 NL 抽2张牌。 NL 丢弃1张牌。";
+    private static final String description2 = "* 预见 !M! 。 NL 丢弃任意张牌，并抽等量的牌。";
     private static final String img_path = CodeUtil.imgPath("card/magician/Phantom_Attack.png");
     private static final CardType type = CardType.SKILL;
     private static final CardColor color = Magician.PlayerColorEnum.Magician_Colour;
-    private static final CardRarity rarity = CardRarity.COMMON;
+    private static final CardRarity rarity = CardRarity.RARE;
     private static final CardTarget target = CardTarget.SELF;
     
     public Cheat()
     {
         super(ID, name, img_path, cost, description1, type, color, rarity, target);
         
-        this.magicNumber = this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber = 4;
     }
 
     @Override
@@ -47,11 +47,11 @@ public class Cheat extends CustomCard
     @Override
     public void use(AbstractPlayer player, AbstractMonster monster)
     {
-        this.addToBot(new ScryAction(5));
-        this.addToBot(new DrawCardAction(player, this.magicNumber));
+        this.addToBot(new ScryAction(this.magicNumber));
         
         if (!this.upgraded)
         {
+            this.addToBot(new DrawCardAction(player, 2));
             this.addToBot(new DiscardAction(player, player, 1, false));
         }
         else
