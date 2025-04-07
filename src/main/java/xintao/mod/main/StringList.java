b96e4c44d111b41e5f9eb4f0a41456bdb9883028
xintao <andymc233@163.com>
2025-04-07 22:39:53 +0800

    25.0407.1

--- src/main/java/xintao/mod/main/StringList.java
+++ src/main/java/xintao/mod/main/StringList.java
@@ -3,6 +3,7 @@ package xintao.mod.main;
 import basemod.BaseMod;
 import basemod.interfaces.EditStringsSubscriber;
 import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import xintao.mod.util.CodeUtil;
 
 public class StringList implements EditStringsSubscriber
@@ -18,5 +19,6 @@ public class StringList implements EditStringsSubscriber
         String lang = "ZHS";
         
         BaseMod.loadCustomStringsFile(RelicStrings.class, CodeUtil.langPath(lang + "/relic.json"));
+        BaseMod.loadCustomStringsFile(UIStrings.class, CodeUtil.langPath(lang + "/ui.json"));
     }
 }
